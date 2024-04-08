114c1cfe2e24ac0b5e0eb3c2aa626b29ebaa2a9b
outphase <leonardo.grisoli@gmail.com>
2024-04-08 18:13:49 +0200

    multi file compilation

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,8 @@ pub mod project {
     use crate::text;
     use std::{
         fs, io,
-        path::Path,
-        process,
-        process::{Command, Output},
+        path::{Path, PathBuf},
+        process::{self, Command, Output},
     };
 
     pub fn new(name: &str) {
@@ -19,11 +18,11 @@ pub mod project {
 
     pub fn debug() {
         let name = current_dir_name();
-        println!("||<><>|| Compiling {name} with debug information.\n");
-        let output = ps_command(
-            format!("clang++ --debug ../main.cpp -o {name}.exe").trim(),
-            "./build",
-        );
+        let file_names = get_compiler_file_names();
+        let command = format!("clang++ --debug {file_names} -o {name}.exe");
+        let output = ps_command(&command.trim(), "./build");
+
+        println!("AAAAAAAAAAAAAAAAAAAA {}", command);
         if let Ok(output) = output {
             print!(
                 "{}",
@@ -32,15 +31,17 @@ pub mod project {
         } else {
             println!("||** WARNING\n||** Could not build {name}");
         };
+        println!("||<><>|| Compiling {name} with debug information.\n");
     }
 
     pub fn release() {
         let name = current_dir_name();
-        println!("||<><>|| Compiling {name}.\n");
+        let file_names = get_compiler_file_names();
         let output = ps_command(
-            format!("clang++ ../main.cpp -o {name}.exe").trim(),
+            format!("clang++ {file_names} -o {name}.exe").trim(),
             "./build",
         );
+
         if let Ok(output) = output {
             print!(
                 "{}",
@@ -49,6 +50,7 @@ pub mod project {
         } else {
             println!("||** WARNING\n||** Could not build {name}");
         };
+        println!("||<><>|| Compiling {name}.\n");
     }
 
     pub fn run() {
@@ -60,8 +62,6 @@ pub mod project {
         );
         let mut exe = Command::new(format!("./build/{name}.exe"));
         let thread = exe.spawn();
-        // let output = exe.output();
-        // let output = ps_command(format!("./build/{name}.exe").trim(), "");
         if let Ok(output) = thread.unwrap().wait_with_output() {
             print!(
                 "{}",
@@ -92,15 +92,54 @@ pub mod project {
             .unwrap()
             .to_string()
     }
+
+    fn get_compiler_files() -> Vec<PathBuf> {
+        if !Path::new("./main.cpp").exists() {
+            println!("||** WARNING\n||** main.cpp not found. Is this a project directory?\n");
+            process::exit(1);
+        }
+        fs::read_dir(".")
+            .unwrap()
+            .map(|res| res.map(|e| e.path()))
+            .filter(|x| x.is_ok())
+            .map(|x| x.unwrap())
+            .filter(|x| x.to_string_lossy().contains(".cpp"))
+            .collect::<Vec<_>>()
+    }
+
+    fn get_compiler_file_names() -> String {
+        let mut file_names = String::new();
+        for file in get_compiler_files() {
+            file_names.push_str("../");
+            file_names.push_str(&file.file_name().unwrap().to_string_lossy());
+            file_names.push_str(" ");
+        }
+        file_names
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::fs;
+
     #[test]
     fn path_name() {
         let dir = std::env::current_dir().unwrap();
         let name = &dir.to_str().unwrap().split("\\").last().unwrap();
 
         assert_eq!(name, &"clout");
+    }
+
+    #[test]
+    fn compiler_files() {
+        let files: Vec<_> = fs::read_dir("./test")
+            .unwrap()
+            .map(|res| res.map(|e| e.path()))
+            .filter(|x| x.is_ok())
+            .map(|x| x.unwrap())
+            .filter(|x| x.to_string_lossy().contains(".cpp"))
+            .collect();
+
+        assert_eq!(files[0].file_name().unwrap().to_string_lossy(), "main.cpp")
     }
 }
